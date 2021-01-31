9ea06b6fdef17ca163adb2b24a81dc19f71dff5e
Alex Waller <alex@artofalexwaller.com>
2021-01-30 21:27:18 -0500

    more controller work

--- app/controllers/motorhomes_controller.rb
+++ app/controllers/motorhomes_controller.rb
@@ -1,2 +1,37 @@
 class MotorhomesController < ApplicationController
+
+    def index
+        @motorhomes = Motorhome.all
+    end
+
+    def show
+        @motorhome = Motorhome.find(params[:id])
+    end
+
+    def new
+        @motorhome = Motorhome.new
+    end
+
+    def create
+        @motorhome = Motorhome.new(motorhome_params)
+        @motorhome.save
+        redirect_to motorhome_path(@motorhome)
+    end
+
+    def edit
+        @motorhome = Motorhome.find(params[:id])
+    end
+
+    def update
+        @motorhome = Motorhome.find(params[:id])
+        @motorhome.update(motorhome_params)
+        redirect_to motorhome_path(@motorhome)
+    end
+
+    private
+
+         def motorhome_params
+            params.require(:motorhome).permit(:model)
+         end
+
 end
