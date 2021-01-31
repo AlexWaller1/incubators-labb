1a7be8748ee01101d52a1daf55da04fdcc2f4728
Alex Waller <alex@artofalexwaller.com>
2021-01-30 21:18:14 -0500

    further controller work

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,36 @@
 class FriendsController < ApplicationController
+
+    def index
+        @friends = Friend.all
+    end
+
+    def show
+        @friend = Friend.find(params[:id])
+    end
+
+    def new
+        @friend = Friend.new
+    end
+
+    def create
+        @friend = Friend.new(friend_params)
+        @friend.save
+        redirect_to friend_path(@friend)
+    end
+
+    def edit
+        @friend = Friend.find(params[:id])
+    end
+
+    def update
+        @friend = Friend.find(params[:id])
+        @friend.update(friend_params)
+        redirect_to friend_path(@friend)
+    end
+
+    private
+
+         def friend_params
+            params.require(:friend).permit(:name)
+         end
 end
--- app/controllers/hybrids_controller.rb
+++ app/controllers/hybrids_controller.rb
@@ -1,2 +1,37 @@
 class HybridsController < ApplicationController
+
+    def index
+        @hybrids = Hybrid.all
+    end
+
+    def show
+        @hybrid = Hybrid.find(params[:id])
+    end
+
+    def new
+        @hybrid = Hybrid.new
+    end
+
+    def create
+        @hybrid = Hybrid.new(hybrid_params)
+        @hybrid.save
+        redirect_to hybrid_path(@hybrid)
+    end
+
+    def edit
+        @hybrid = Hybrid.find(params[:id])
+    end
+
+    def update
+        @hybrid = Hybrid.find(params[:id])
+        @hybrid.update(hybrid_params)
+        redirect_to hybrid_path(@hybrid)
+    end
+
+    private
+
+         def hybrid_params
+            params.require(:hybrid).permit(:name)
+         end
+
 end
