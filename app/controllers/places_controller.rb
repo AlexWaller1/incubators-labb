09a9c513698443ccb4c0062d77c30b4d6dda721a
Alex Waller <alex@artofalexwaller.com>
2021-01-30 21:41:13 -0500

    more controller work

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,37 @@
 class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+    end
+
+    def show
+        @place = Place.find(params[:id])
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(place_params)
+        @place.save
+        redirect_to place_path(@place)
+    end
+
+    def edit
+        @place = Place.find(params[:id])
+    end
+
+    def update
+        @place = Place.find(params[:id])
+        @place.update(place_params)
+        redirect_to place_path(@place)
+    end
+
+    private
+
+         def place_params
+            params.require(:place).permit(:name)
+         end
+
 end
