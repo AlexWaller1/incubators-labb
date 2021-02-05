98f0deaa4260c5ba0465da33910e498a22cb0592
Alex Waller <alex@artofalexwaller.com>
2021-02-04 21:44:08 -0500

    added user security

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -22,14 +22,34 @@ class FriendsController < ApplicationController
 
     def edit
         @friend = Friend.find(params[:id])
+        if current_user == @friend.hybrid.user
+            render :edit
+        else
+            redirect_to hybrid_friend_path(@friend.hybrid, @friend)
+        end
     end
 
     def update
         @friend = Friend.find(params[:id])
-        if @friend.update(friend_params)
-         redirect_to hybrid_friend_path(hybrid_id: @friend.hybrid_id, id: @friend.id)
-        else
+        if current_user == @friend.hybrid.user
+         if @friend.update(friend_params)
+          redirect_to hybrid_friend_path(hybrid_id: @friend.hybrid_id, id: @friend.id)
+         else
          render :edit
+         end
+        else
+            redirect_to hybrid_friend_path(@friend.hybrid, @friend)
+        end
+    end
+
+    def destroy
+        @friend = Friend.find(params[:id])
+        if current_user == @friend.hybrid.user
+          @friend.destroy
+          flash[:notice] = "Friend Scrubbed from Database"
+          redirect_to hybrid_friends_path
+        else
+          redirect_to hybrid_friends_path
         end
     end
 
