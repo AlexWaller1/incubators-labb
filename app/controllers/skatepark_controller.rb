21c0b190e0a6a403ae43850d8abf0e492935fee9
Alex Waller <alex@artofalexwaller.com>
2021-02-28 03:05:30 -0500

    initial commit

--- app/controllers/skatepark_controller.rb
+++ app/controllers/skatepark_controller.rb
@@ -1,24 +1,34 @@
 class SkateparkController < ApplicationController
 
     def index
+        @skateparks = Skatepark.all
     end
 
     def show
+        @skatepark = Skatepark.find_by(id: params[:id])
     end
 
     def new
+        @skatepark = Skatepark.new(user: current_user)
     end
 
     def create
+        @skatepark = Skatepark.find(params[:id])
+        @skatepark.create
     end
 
     def edit
+        @skatepark = Skatepark.find(params[:id])
     end
 
     def update
+        @skatepark = Skatepark.find(params[:id])
+        @skatepark.update
     end
 
     def destroy
+        @skatepark = Skatepark.find(params[:id])
+        @skatepark.destroy
     end
 
     private
