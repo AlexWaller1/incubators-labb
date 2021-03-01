ff48b88c05b3bc8231ce3c7d055ca32bb686dcd5
Alex Waller <alex@artofalexwaller.com>
2021-02-28 23:00:09 -0500

    robots industry controller actions

--- app/controllers/industry_centers_controller.rb
+++ app/controllers/industry_centers_controller.rb
@@ -1,29 +1,40 @@
 class IndustryCentersController < ApplicationController
 
     def index
+        @industry_centers = IndustryCenter.all
     end
 
     def show
+        @industry_center = IndustryCenter.find_by(id: params[:id])
     end
 
     def new
+        @industry_center = IndustryCenter.new(user: current_user)
     end
 
     def create
+        @industry_center = IndustryCenter.new(industry_center_params)
+        @industry_center.save
     end
 
     def edit
+        @industry_center = IndustryCenter.find(params[:id])
     end
 
     def update
+        @industry_center = IndustryCenter.find(params[:id])
+        @industry.update(industry_center_params)
     end
 
     def destroy
+        @industry_center = IndustryCenter.find(params[:id])
+        @industry_center.destroy
     end
 
     private
 
-    def industry_centers_params
+    def industry_center_params
+        params.require(:industry_center).require(:name, :city, :state, :performance, :image, :future_goals, :user_id)
     end
 
 end
